261974023d7ec10ae70c77e4a6c4797cc3832d76
dev-cmyk <66713052+dev-cmyk@users.noreply.github.com>
2020-06-10 17:13:46 +0800

    Update customizable.js

--- customizable.js
+++ customizable.js
@@ -3,9 +3,9 @@ var x = window.matchMedia("(max-width: 1024px)")
 $( document ).ready(function() {
     
   $("#contact_phone").addClass("w-100");
-  $("#contact_question").addClass("w-100, btn-astra-gold");
+  $("#contact_question").addClass("w-100");
   
-  $("input.button.button-primary").addClass("w-100");
+  $("input.button.button-primary").addClass("w-100 btn-astra-gold");
     
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes
