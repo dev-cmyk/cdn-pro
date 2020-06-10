3367bd3321233fa344c62b13ea1754af02833a5c
dev-cmyk <66713052+dev-cmyk@users.noreply.github.com>
2020-06-10 16:56:49 +0800

    Update customizable.js

--- customizable.js
+++ customizable.js
@@ -1,6 +1,15 @@
 var fc = document.getElementById("fixed-column");
 var x = window.matchMedia("(max-width: 1024px)")
 
+$( document ).ready(function() {
+    
+  $("div[id^='contact']").addClass("w-100");
+  
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
+  
+});
+
 function myFunction(x) {
   if (x.matches) { // If media query matches
     fc.classList.remove("position-fixed");
@@ -8,7 +17,3 @@ function myFunction(x) {
     fc.classList.add("position-fixed");
   }
 }
-
-
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
