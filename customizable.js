ce903ec948e837dc1020dc24d1a7d74fab23f905
dev-cmyk <66713052+dev-cmyk@users.noreply.github.com>
2020-06-10 14:32:34 +0800

    Update customizable.js

--- customizable.js
+++ customizable.js
@@ -1,7 +1,13 @@
 var fixed_column = document.getElementById("fixed-column");
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  fixed_column.classList.remove("position-fixed");
-} else {
-  fixed_column.classList.add("position-fixed");
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    fixed_column.classList.remove("position-fixed");
+  } else {
+    fixed_column.classList.remove("position-fixed");
+  }
 }
+
+var x = window.matchMedia("(max-width: 1024px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
